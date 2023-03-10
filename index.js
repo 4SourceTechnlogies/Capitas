8bb53dfac878752796006f8a72816e9d1ff8bc11
Four Source Technologies <118180580+4SourceTechnlogies@users.noreply.github.com>
2023-03-10 16:12:27 +0530

    added new effect in prtfolio

---
+++ index.js
@@ -0,0 +1,19 @@
+var portImg = document.querySelectorAll('.portfolioImg');
+portImg.forEach((pImg)=> pImg.addEventListener('mouseenter', function(){
+    
+    for (var i =0; i <= portImg.length; i++){
+        portImg[i].classList.remove('active');
+        this.classList.add("active");
+    }
+}))
+
+portImg.forEach((pImg) =>
+  pImg.addEventListener("mouseleave", function () {
+    if (this.classList.contains("active")){
+        this.classList.remove('active')
+    }
+  })
+);
+
+
+ 
